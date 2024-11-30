8289617639c597fe8aa0c2962e859ad460c10c5b
Najman Husaini <najmanhusaini20@gmail.com>
2024-11-30 13:31:32 -0700

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,14 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+export default {
 	kit: {
 		adapter: adapter({
 			images: {
 				sizes: [640, 828, 1200, 1920, 3840],
 				domains: []
-			},
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+			}
+		})
+	},
+	preprocess: vitePreprocess()
 };
-
-export default config;
