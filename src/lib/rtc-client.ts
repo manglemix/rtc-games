a4206331b9b104ac0a236fab747fc2f2fae046ec
Najman Husaini <najmanhusaini20@gmail.com>
2024-11-30 19:14:35 -0700

    fixes

--- src/lib/rtc-client.ts
+++ src/lib/rtc-client.ts
@@ -4,19 +4,16 @@ export type SdpAnswer = { sdp: RTCSessionDescriptionInit; ices: RTCIceCandidate[
 class NetworkPeer {
 	private rtc: RTCPeerConnection = new RTCPeerConnection({
 		iceServers: [
-			{
-				urls: ['stun:stun.l.google.com:19302']
-			}
-			// {
-			// 	urls: ['turn:192.158.29.39:3478?transport=udp'],
-			// 	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			// 	username: '28224511:1379330808'
-			// },
-			// {
-			// 	urls: ['turn:192.158.29.39:3478?transport=tcp'],
-			// 	'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-			// 	'username': '28224511:1379330808'
-			// }
+			{ urls: "stun:stun.l.google.com:19302" },
+			{ urls: "stun:stun.l.google.com:5349" },
+			{ urls: "stun:stun1.l.google.com:3478" },
+			{ urls: "stun:stun1.l.google.com:5349" },
+			{ urls: "stun:stun2.l.google.com:19302" },
+			{ urls: "stun:stun2.l.google.com:5349" },
+			{ urls: "stun:stun3.l.google.com:3478" },
+			{ urls: "stun:stun3.l.google.com:5349" },
+			{ urls: "stun:stun4.l.google.com:19302" },
+			{ urls: "stun:stun4.l.google.com:5349" }
 		]
 	});
 	private rtcConnChannel: RTCDataChannel | null = null;
